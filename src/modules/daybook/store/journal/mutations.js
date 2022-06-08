34a4c9ebb4899e4c9c9f33cd7fbe5f8a6f2c94bb
Ian Mercadal <imercadal@cifpfbmoll.eu>
2022-06-08 18:16:41 +0200

    feat: :art: Vuex Modules
    Creando base y correción de fallo en commit anterior

---
+++ src/modules/daybook/store/journal/mutations.js
@@ -0,0 +1,12 @@
+// export const myAction = (state) => {
+
+// }
+export const setEntries = (/*state*/) => {
+
+}
+export const updateEntries = (/*state*/) => {
+
+}
+export const addEntries = (/*state*/) => {
+
+}
