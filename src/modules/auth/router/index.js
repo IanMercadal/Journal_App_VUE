69f42c85196030f454b24f4295988fcb0c61b456
Ian Mercadal <imercadal@cifpfbmoll.eu>
2022-07-11 19:47:14 +0200

    feat: :art: Rutas de Login y register
    Creando rutas y páginas de usuario

--- src/modules/auth/router/index.js
+++ src/modules/auth/router/index.js
@@ -1,5 +1,16 @@
 export default {
     name: 'auth',
     component: () => import(/* webpackChunkName: "" */ '@/modules/auth/layouts/AuthLayout.vue'),
-    children: []
+    children: [
+        {
+            path: '',
+            name: 'login',
+            component: () => import(/* webpackChunkName: "" */ '@/modules/auth/views/Login.vue'),
+        },
+        {
+            path: '/register',
+            name: 'register',
+            component: () => import(/* webpackChunkName: "" */ '@/modules/auth/views/Register.vue'),
+        }
+    ]
 }
